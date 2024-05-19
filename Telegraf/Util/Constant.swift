8ef7aa6400897f92daba1f4592528ed7b6b73f65
Natasha Radika <81007696+tashagrc@users.noreply.github.com>
2024-05-19 14:05:57 +0700

    add landing page and color

--- Telegraf/Util/Constant.swift
+++ Telegraf/Util/Constant.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+
+//-- Lora-Regular
+//-- Lora-Italic
+//-- Lora-Regular_Medium
+//-- Lora-Italic_Medium-Italic
+//-- Lora-Regular_SemiBold
+//-- Lora-Italic_SemiBold-Italic
+//-- Lora-Regular_Bold
+//-- Lora-Italic_Bold-Italic
+
+enum Constant : String {
+    case regular_light = "Lora-Regular"
+    case italic_light = "Lora-Italic"
+    
+    case regular_medium = "Lora-Regular_Medium"
+    case italic_medium = "Lora-Italic_Medium-Italic"
+    
+    case regular_semibold = "Lora-Regular_SemiBold"
+    case italic_semibold = "Lora-Italic_SemiBold-Italic"
+    
+    case regular_bold = "Lora-Regular_Bold"
+    case italic_bold = "Lora-Italic_Bold-Italic"
+}
--- Telegraf/View/ContentView.swift
+++ Telegraf/View/ContentView.swift
@@ -15,16 +15,47 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        
         NavigationStack {
             ZStack {
                 Color("Black")
                             .ignoresSafeArea()
-                VStack {
-                    Text("Telegraf")
-                        .font(.custom(Constant.italic_bold.rawValue, size: 56)).foregroundColor(Color("Red"))
-                    Text("Chat Like It's the 1800s!")
-                        .font(.custom(Constant.regular_light.rawValue, size: 16)).foregroundColor(Color("Cream"))
+                
+                VStack(spacing: 120){
+                    VStack(spacing: 14) {
+                        Text("Telegraf")
+                            .font(.custom(Constant.italic_bold.rawValue, size: 56)).foregroundColor(Color("Red"))
+                        
+                        Text("Chat Like It's the 1800s!")
+                            .font(.custom(Constant.regular_light.rawValue, size: 16)).foregroundColor(Color("Cream"))
+                    }
+                    
+                    
                     Image("telegraf_landing")
+                    
+                    VStack(spacing: 28) {
+                        // button and nav link
+                        NavigationLink(destination: {
+                            
+                        }, label: {
+                            Text("Onboard")
+                                .font(.custom(Constant.regular_light.rawValue, size: 32)).foregroundColor(Color("Red")).padding(.horizontal, 80)
+                                .padding(.vertical, 10)
+                                .background(
+                                    RoundedRectangle(cornerRadius: 20)
+                                        .stroke(Color("Red"), lineWidth: 2)
+                                )
+                        })
+                        
+                        NavigationLink(destination: {
+                            PlaygroundView()
+                        }, label: {
+                            Text("Go to playground")
+                                .font(.custom(Constant.regular_light.rawValue, size: 16)).foregroundColor(Color("Cream"))
+                                .underline()
+                        })
+                    }
+                    
                     
                 }
                 .padding()
